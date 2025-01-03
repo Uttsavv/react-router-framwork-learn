8ee11d9032398ecdfc890bf67211b45df3e161c8
Utsav Singh <89920027+Utsav-Singh-01@users.noreply.github.com>
2025-01-04 01:47:51 +0530

    add: drizzle zod and create-user

--- app/routes/create-user.tsx
+++ app/routes/create-user.tsx
@@ -1,14 +1,27 @@
 import { useFetcher } from "react-router";
+import type { Route } from "./+types/create-users";
 
-export async function action() {}
+export async function action({request}: Route.LoaderArgs) {
+    const formData = 
+}
 
 export default function CreateUser() {
     const fetcher = useFetcher();
     return (
         <div>
-            <fetcher.Form>
-                <label htmlFor="username" />
-                <input type="text" id="username" />
+            <fetcher.Form method="POST">
+                <div>
+                    <label htmlFor="name" />
+                    <input type="text" id="name" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="age" />
+                    <input type="number" id="age" name="age" />
+                </div>
+                <div>
+                    <label htmlFor="email" />
+                    <input type="text" id="email" name="email" />
+                </div>
             </fetcher.Form>
         </div>
     );
